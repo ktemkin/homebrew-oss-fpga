8ff129b8c8d045b86cbf11316e31485343600aae
Kate Temkin <k@ktemkin.com>
2019-04-10 02:25:28 -0600

    remove the ice40-tools metapackage, as homebrew doesn't really like those

--- ice40-tools.rb
+++
@@ -1,9 +0,0 @@
-class Ice40Tools < Formula
-  desc "tools for documenting and working with iCE40 FPGA files"
-  homepage "https://github.com/cliffordwolf/icestorm"
-  head "https://github.com/cliffordwolf/icestorm.git"
-
-  depends_on "yosys"
-  depends_on "icestorm"
-  depends_on "nextpnr-ice40"
-end
