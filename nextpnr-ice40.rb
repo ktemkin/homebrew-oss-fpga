0fb9ddb273c47667fa96eda47ce7220939a659b1
Kate Temkin <k@ktemkin.com>
2019-09-19 03:57:42 -0600

    ice40: update recipe to handle cmake/boost changes

--- nextpnr-ice40.rb
+++ nextpnr-ice40.rb
@@ -13,7 +13,7 @@ class NextpnrIce40 < Formula
   depends_on "icestorm"
 
   def install
-    system "cmake", "-DARCH=ice40", ".", *std_cmake_args
+    system "cmake", "-DARCH=ice40", ".", *std_cmake_args, "-DBoost_NO_BOOST_CMAKE=on", "-DBUILD_TESTS=OFF"
     system "make", "install"
   end
 
